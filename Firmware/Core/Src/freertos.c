8b23ddbcbe2b10f4e38be22edb14599c61ea1f59
katodo <mauro.soligo@katodo.com>
2022-10-27 16:34:23 +0200

    test with publishing o different messages... FAIL...

--- Firmware/Core/Src/freertos.c
+++ Firmware/Core/Src/freertos.c
@@ -35,6 +35,12 @@
 #include <rmw_microros/rmw_microros.h>
 
 #include <std_msgs/msg/int32.h>
+#include <std_msgs/msg/int64.h>
+#include <std_msgs/msg/color_rgba.h>
+//#include <nav_msgs/msg/odometry.h>
+#include <sensor_msgs/msg/battery_state.h>
+#include <sensor_msgs/msg/temperature.h>
+#include <std_msgs/msg/float64_multi_array.h>
 
 #include "usart.h"
 /* USER CODE END Includes */
@@ -59,31 +65,29 @@ typedef StaticTask_t osStaticThreadDef_t;
 /* USER CODE BEGIN Variables */
 
 /* USER CODE END Variables */
-/* Definitions for blink01 */
-osThreadId_t blink01Handle;
-const osThreadAttr_t blink01_attributes = {
-  .name = "blink01",
-  .stack_size = 128 * 4,
+/* Definitions for rosTaskLed */
+osThreadId_t rosTaskLedHandle;
+uint32_t rosTaskLedBuffer[ 512 ];
+osStaticThreadDef_t rosTaskLedControlBlock;
+const osThreadAttr_t rosTaskLed_attributes = {
+  .name = "rosTaskLed",
+  .cb_mem = &rosTaskLedControlBlock,
+  .cb_size = sizeof(rosTaskLedControlBlock),
+  .stack_mem = &rosTaskLedBuffer[0],
+  .stack_size = sizeof(rosTaskLedBuffer),
   .priority = (osPriority_t) osPriorityBelowNormal,
 };
-/* Definitions for blink02 */
-osThreadId_t blink02Handle;
-const osThreadAttr_t blink02_attributes = {
-  .name = "blink02",
-  .stack_size = 128 * 4,
-  .priority = (osPriority_t) osPriorityBelowNormal,
-};
-/* Definitions for defaultTask */
-osThreadId_t defaultTaskHandle;
-uint32_t defaultTaskBuffer[ 3000 ];
-osStaticThreadDef_t defaultTaskControlBlock;
-const osThreadAttr_t defaultTask_attributes = {
-  .name = "defaultTask",
-  .cb_mem = &defaultTaskControlBlock,
-  .cb_size = sizeof(defaultTaskControlBlock),
-  .stack_mem = &defaultTaskBuffer[0],
-  .stack_size = sizeof(defaultTaskBuffer),
-  .priority = (osPriority_t) osPriorityNormal,
+/* Definitions for rosTaskCom */
+osThreadId_t rosTaskComHandle;
+uint32_t rosTaskComBuffer[ 12000 ];
+osStaticThreadDef_t rosTaskComControlBlock;
+const osThreadAttr_t rosTaskCom_attributes = {
+  .name = "rosTaskCom",
+  .cb_mem = &rosTaskComControlBlock,
+  .cb_size = sizeof(rosTaskComControlBlock),
+  .stack_mem = &rosTaskComBuffer[0],
+  .stack_size = sizeof(rosTaskComBuffer),
+  .priority = (osPriority_t) osPriorityAboveNormal,
 };
 
 /* Private function prototypes -----------------------------------------------*/
@@ -99,9 +103,8 @@ void * microros_reallocate(void * pointer, size_t size, void * state);
 void * microros_zero_allocate(size_t number_of_elements, size_t size_of_element, void * state);
 /* USER CODE END FunctionPrototypes */
 
-void StartBlink01(void *argument);
-void StartBlink02(void *argument);
-void StartDefaultTask(void *argument);
+void StartTaskLed(void *argument);
+void StartTaskCom(void *argument);
 
 extern void MX_USB_DEVICE_Init(void);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
@@ -133,14 +136,11 @@ void MX_FREERTOS_Init(void) {
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
-  /* creation of blink01 */
-  blink01Handle = osThreadNew(StartBlink01, NULL, &blink01_attributes);
+  /* creation of rosTaskLed */
+  rosTaskLedHandle = osThreadNew(StartTaskLed, NULL, &rosTaskLed_attributes);
 
-  /* creation of blink02 */
-  blink02Handle = osThreadNew(StartBlink02, NULL, &blink02_attributes);
-
-  /* creation of defaultTask */
-  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+  /* creation of rosTaskCom */
+  rosTaskComHandle = osThreadNew(StartTaskCom, NULL, &rosTaskCom_attributes);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -152,115 +152,144 @@ void MX_FREERTOS_Init(void) {
 
 }
 
-/* USER CODE BEGIN Header_StartBlink01 */
+/* USER CODE BEGIN Header_StartTaskLed */
 /**
-  * @brief  Function implementing the blink01 thread.
+  * @brief  Function implementing the rosTaskLed thread.
   * @param  argument: Not used
   * @retval None
   */
-/* USER CODE END Header_StartBlink01 */
-void StartBlink01(void *argument)
+/* USER CODE END Header_StartTaskLed */
+void StartTaskLed(void *argument)
 {
   /* init code for USB_DEVICE */
   MX_USB_DEVICE_Init();
-  /* USER CODE BEGIN StartBlink01 */
-  /* Infinite loop */
-  for(;;)
-  {   HAL_GPIO_TogglePin(O_LED_D2_GPIO_Port, O_LED_D2_Pin);
-  	  osDelay(100);
-  }
-  // In case we accidentally exit from task loop
-  osThreadTerminate(NULL);
-  /* USER CODE END StartBlink01 */
-}
-
-/* USER CODE BEGIN Header_StartBlink02 */
-/**
-* @brief Function implementing the blink02 thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_StartBlink02 */
-void StartBlink02(void *argument)
-{
-  /* USER CODE BEGIN StartBlink02 */
+  /* USER CODE BEGIN StartTaskLed */
   /* Infinite loop */
   for(;;)
   {
-	    HAL_GPIO_TogglePin(O_LED_D3_GPIO_Port, O_LED_D3_Pin);
-	    osDelay(600);
+		HAL_GPIO_WritePin(O_LED_D2_GPIO_Port, O_LED_D2_Pin, 1);
+		osDelay(100);
+		HAL_GPIO_WritePin(O_LED_D2_GPIO_Port, O_LED_D2_Pin, 0);
+		osDelay(200);
   }
-
-  // In case we accidentally exit from task loop
-  osThreadTerminate(NULL);
-  /* USER CODE END StartBlink02 */
+  /* USER CODE END StartTaskLed */
 }
 
-/* USER CODE BEGIN Header_StartDefaultTask */
+/* USER CODE BEGIN Header_StartTaskCom */
 /**
-* @brief Function implementing the defaultTask thread.
+* @brief Function implementing the rosTaskCom thread.
 * @param argument: Not used
 * @retval None
 */
-/* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument)
+/* USER CODE END Header_StartTaskCom */
+void StartTaskCom(void *argument)
 {
-  /* USER CODE BEGIN StartDefaultTask */
-	  rmw_uros_set_custom_transport(
-	    true,
-	    (void *) &huart1,
-	    cubemx_transport_open,
-	    cubemx_transport_close,
-	    cubemx_transport_write,
-	    cubemx_transport_read);
+  /* USER CODE BEGIN StartTaskCom */
+	rmw_uros_set_custom_transport(
+		true,
+		(void *) &huart1,
+		cubemx_transport_open,
+		cubemx_transport_close,
+		cubemx_transport_write,
+		cubemx_transport_read);
 
-	  rcl_allocator_t freeRTOS_allocator = rcutils_get_zero_initialized_allocator();
-	  freeRTOS_allocator.allocate = microros_allocate;
-	  freeRTOS_allocator.deallocate = microros_deallocate;
-	  freeRTOS_allocator.reallocate = microros_reallocate;
-	  freeRTOS_allocator.zero_allocate =  microros_zero_allocate;
+	rcl_allocator_t freeRTOS_allocator = rcutils_get_zero_initialized_allocator();
+	freeRTOS_allocator.allocate = microros_allocate;
+	freeRTOS_allocator.deallocate = microros_deallocate;
+	freeRTOS_allocator.reallocate = microros_reallocate;
+	freeRTOS_allocator.zero_allocate =  microros_zero_allocate;
 
-	  if (!rcutils_set_default_allocator(&freeRTOS_allocator)) {
-	      printf("Error on default allocators (line %d)\n", __LINE__);
-	  }
+	if (!rcutils_set_default_allocator(&freeRTOS_allocator))
+	{	printf("Error on default allocators (line %d)\n", __LINE__);
+	}
 
-	  // micro-ROS app
+	// micro-ROS app
+	rcl_publisher_t publisher0;
+	std_msgs__msg__Int32 msg0;
 
-	  rcl_publisher_t publisher;
-	  std_msgs__msg__Int32 msg;
-	  rclc_support_t support;
-	  rcl_allocator_t allocator;
-	  rcl_node_t node;
+	rcl_publisher_t publisher1;
+	std_msgs__msg__Int64 msg1;
 
-	  allocator = rcl_get_default_allocator();
+	rcl_publisher_t publisher2;
+	sensor_msgs__msg__BatteryState msg2;
 
-	  //create init_options
-	  rclc_support_init(&support, 0, NULL, &allocator);
+	rcl_publisher_t publisher3;
+	sensor_msgs__msg__Temperature msg3;
 
-	  // create node
-	  rclc_node_init_default(&node, "cubemx_node", "", &support);
+	rcl_publisher_t publisher4;
+	std_msgs__msg__Float64MultiArray msg4;
 
-	  // create publisher
-	  rclc_publisher_init_default(
-	    &publisher,
-	    &node,
-	    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-	    "cubemx_publisher");
+	rcl_publisher_t publisher5;
+	std_msgs__msg__ColorRGBA msg5;
 
-	  msg.data = 0;
 
-	  for(;;)
-	  {
-	    rcl_ret_t ret = rcl_publish(&publisher, &msg, NULL);
-	    if (ret != RCL_RET_OK)
-	    {
-	      printf("Error publishing (line %d)\n", __LINE__);
-	    }
+	rclc_support_t support;
+	rcl_allocator_t allocator;
+	rcl_node_t node;
 
-	    msg.data++;
-	    osDelay(10);
-	  }
-  /* USER CODE END StartDefaultTask */
+	allocator = rcl_get_default_allocator();
+
+	//create init_options
+	rclc_support_init(&support, 0, NULL, &allocator);
+
+	// create node
+	rclc_node_init_default(&node, "pnav32", "", &support);
+
+	// create publisher
+	rclc_publisher_init_default( &publisher0, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "pInt32");
+	rclc_publisher_init_default( &publisher1, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int64), "pInt64");
+	rclc_publisher_init_default( &publisher2, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, BatteryState), "pBatt");
+	rclc_publisher_init_default( &publisher3, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Temperature), "pTemp");
+	rclc_publisher_init_default( &publisher4, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Temperature), "pFloat64M");
+	rclc_publisher_init_default( &publisher5, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Temperature), "pColorRGBA");
+
+	// preinit with random test value
+	msg0.data = 1;
+	msg1.data = 10;
+	msg2.voltage = 24.9;
+	msg2.charge = 35;
+	msg2.current = 10;
+	msg3.temperature = 45;
+	msg5.r= 127;
+	msg5.g= 127;
+	msg5.b= 100;
+	msg5.a= 200;
+
+  for(;;)
+  {
+
+	// Random data update before publish
+	msg0.data++;
+	msg1.data++;
+	msg2.voltage += 0.001;
+	msg3.temperature += 0.001;
+	msg5.g++;
+
+
+	// Led ON
+	HAL_GPIO_WritePin(O_LED_D3_GPIO_Port, O_LED_D3_Pin, 0);
+
+	rcl_ret_t ret;
+	ret = rcl_publish(&publisher0, &msg0, NULL);
+	ret += rcl_publish(&publisher1, &msg1, NULL);
+//	ret += rcl_publish(&publisher2, &msg2, NULL);
+//	ret += rcl_publish(&publisher3, &msg3, NULL);
+//	ret += rcl_publish(&publisher4, &msg4, NULL);
+//	ret += rcl_publish(&publisher4, &msg4, NULL);
+
+	if (ret != RCL_RET_OK)
+	{
+	  printf("Error publishing (line %d)\n", __LINE__);
+	}
+
+	// Led turn Off
+	HAL_GPIO_WritePin(O_LED_D3_GPIO_Port, O_LED_D3_Pin, 1);
+	osDelay(100);
+  }
+
+
+
+  /* USER CODE END StartTaskCom */
 }
 
 /* Private application code --------------------------------------------------*/
